5572ba520232ec3bd298c82a8eae5680a8ae0670
Cunha <cunhanik1@gmail.com>
2023-09-24 12:40:33 +0100

    fix pagination

--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -45,11 +45,16 @@ export const Pagination = ({
               <a
                 onClick={() => setCurrentPage(page)}
                 href="#"
-                className={`${
+                style={
                   page === currentPage
-                    ? "text-blue-800 font-bold bg-blue-200 hover:text-blue-800 hover:bg-blue-200 "
-                    : ""
-                } flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
+                    ? {
+                        color: "blue",
+                        fontWeight: "bold",
+                        backgroundColor: "#CBD5E1",
+                      }
+                    : {}
+                }
+                className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
               >
                 {page}
               </a>
