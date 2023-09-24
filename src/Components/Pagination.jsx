9b370b576824a96720b8dfdbe5b6a180853ebdc5
Cunha <cunhanik1@gmail.com>
2023-09-24 12:31:17 +0100

    fix pagination colors

--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -47,7 +47,7 @@ export const Pagination = ({
                 href="#"
                 className={`${
                   page === currentPage
-                    ? "text-blue-700 font-bold bg-blue-100 hover:text-blue-700 hover:bg-blue-100 "
+                    ? "text-blue-800 font-bold bg-blue-200 hover:text-blue-800 hover:bg-blue-200 "
                     : ""
                 } flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
               >
